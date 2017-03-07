03d346ecc7d48691d8d6f89542f6e0d37537753b
Eric Sullivan <eric.sullivan@annkissam.com>
2017-03-07 13:24:41 -0500

    initial release

--- mix.exs
+++ mix.exs
@@ -1,37 +1,32 @@
 defmodule Curator.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/curator-ex/curator"
+  @maintainers [
+    "Eric Sullivan",
+  ]
+
   def project do
-    [app: :curator,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps,
-     dialyzer: [plt_add_deps: :project],
+    [
+      app: :curator,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      description: "An Authentication and User Lifecycle Framework for Phoenix",
+      package: package(),
+      source_url: @url,
+      homepage_url: @url,
+      dialyzer: [plt_add_deps: :project],
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :timex, :timex_ecto, :tzdata]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:credo, "~> 0.5", only: [:dev, :test]},
@@ -42,6 +37,16 @@ defmodule Curator.Mixfile do
       {:plug, "~> 1.2"},
       {:timex, "~> 3.0"},
       {:timex_ecto, "~> 3.0"},
+    ]
+  end
+
+  defp package do
+    [
+      name: :curator,
+      maintainers: @maintainers,
+      licenses: ["MIT"],
+      links: %{github: @url},
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*", "CHANGELOG*"],
     ]
   end
 end
